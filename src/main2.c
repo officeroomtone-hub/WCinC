2b1f35098021d2e6c7af8c1b4b343549361ce959
leo <office.roomtone@gmail.com>
2026-08-16 15:49:12 +0200

    renamed variables for better code clarity and readability

--- src/main2.c
+++ src/main2.c
@@ -19,12 +19,12 @@ typedef struct {
 } flags;
 
 int main(int argc, char *argv[]) {
-  int argSize_in_bit(int *argSize);
-  int opt;
-  char arg[1024];
+  int argument_size_in_bits(int *argument_size);
+  int option;
+  char input_argument[1024];
 
-  while ((opt = getopt(argc, argv, "cmlw")) != -1) {
-    switch (opt) {
+  while ((option = getopt(argc, argv, "cmlw")) != -1) {
+    switch (option) {
     case 'c':
       flag_c();
       break;
@@ -45,19 +45,19 @@ int main(int argc, char *argv[]) {
     // printf("sizeof(char) = %d\n",sizeofChar );
     printf("wc clone starting up...\n");
 
-    fgets(arg, sizeof(arg), stdin);
-    arg[strcspn(arg, "\n")] = '\0';
+    fgets(input_argument, sizeof(input_argument), stdin);
+    input_argument[strcspn(input_argument, "\n")] = '\0';
 
     while (1) {
 
-      if (strcmp(arg, "exit") == 0) {
+      if (strcmp(input_argument, "exit") == 0) {
         printf(exit_message);
         return EXIT_SUCCESS;
       }
 
-      int argsize = strlen(arg);
-      printf("size of arg is %d bytes or %d bits\n", argsize,
-             argSize_in_bit(&argsize));
+      int argument_size = strlen(input_argument);
+      printf("size of arg is %d bytes or %d bits\n", argument_size,
+             argument_size_in_bits(&argument_size));
     }
   }
 
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
     printf("%s\n and sizeof %d\n", argv[1], argc - 1);
   }
 
-  if (opt == 'l') {
+  if (option == 'l') {
 
   } else {
     printf("%s\n", "Invalid Input: Unsupported Flag");
@@ -81,4 +81,4 @@ void flag_m() { printf("%s\n", "flag is -m"); }
 void flag_l() { printf("%s\n", "flag is -l"); }
 void flag_w() { printf("%s\n", "flag is -w"); }
 
-int argSize_in_bit(int *argSize) { return *argSize * 8; }
+int argument_size_in_bits(int *argument_size) { return *argument_size * 8; }
