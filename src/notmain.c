6d5c7d96f64c4881aff566f94e716ec902db5bd6
leo <office.roomtone@gmail.com>
2026-08-16 11:29:55 +0200

    moooore rewrition new file bois

--- src/notmain.c
+++ src/notmain.c
@@ -1,26 +1,24 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 void calc_distance(uint8_t, uint8_t);
 
 typedef struct {
   uint8_t An;
   uint8_t Bn;
-}distance;
+} distance;
 
-int main(){
-
-   distance d = {
-     10,
-     34,
-   };
+int main() {
+  distance d = {
+      10,
+      34,
+  };
 
   calc_distance(d.An, d.Bn);
-
 }
 
-  void calc_distance(uint8_t A, uint8_t B) {
-    uint8_t distance_between_points = B - A;
-    printf("%d", distance_between_points);
-  }
+void calc_distance(uint8_t A, uint8_t B) {
+  uint8_t distance_between_points = B - A;
+  printf("%d", distance_between_points);
+}
